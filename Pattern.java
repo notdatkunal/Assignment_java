7845eceb96d8c6e7654ab5776bdd572e9bbf42d1
Kunal Dabhane <kunaldabhane20012@gmail.com>
2022-12-20 20:33:43 +0530

    pattern_file_added

--- Pattern.java
+++ Pattern.java
@@ -1,1 +1,16 @@
-import 
+import java.util.Scanner;
+public class Pattern{
+	public static void main(String[] args){
+		int i,j,length;
+		Scanner in = new Scanner(System.in);
+		System.out.println("enter your length");
+		length = in.nextInt();
+		for(i=0;i<=length;i++){
+			for(j=1;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+// this is a basic pattern boilerplate program
+	}
+}
