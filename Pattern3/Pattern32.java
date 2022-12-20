f4a1b65027588462d127ea59fd49c8ca2b89c373
Kunal Dabhane <kunaldabhane20012@gmail.com>
2022-12-20 23:06:38 +0530

    added pattern32

--- Pattern3/Pattern32.java
+++ Pattern3/Pattern32.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class Pattern31 {
+public class Pattern32 {
 	public static void main(String[] args) {
 		int i, j, length;
 		Scanner in = new Scanner(System.in);
 		System.out.println("enter your length");
 		length = in.nextInt();
-		for (i = 0; i <= length; i++) {
-			for (j = 1; j <= length - i; j++) {
-				System.out.print(j+" ");
+		for (i = 1; i <= length; i++) {
+			for (j = 0; j <= length - i; j++) {
+				System.out.print(i + " ");
 			}
 			System.out.println();
 		}
