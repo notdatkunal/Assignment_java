05948b8bba2935b39978894924e98d56c3f809cb
Kunal Dabhane <kunaldabhane20012@gmail.com>
2022-12-21 14:10:31 +0530

    updated pattern5

--- Pattern4/Pattern57.java
+++ Pattern4/Pattern57.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class Pattern56 {
+public class Pattern57 {
 	public static void main(String[] args) {
 		int i, j, length;
 		Scanner in = new Scanner(System.in);
 		System.out.println("enter your length");
 		length = in.nextInt() + 1;
-		char a = 'a';
-		int b = 1;
+
+		int b = 0;
 		char c = (char) (b + '0');
 		char d = (char) (b + '1');
 		for (i = 0; i <= length; i++) {
@@ -16,9 +16,9 @@ public class Pattern56 {
 			}
 			for (j = length - i; j != 0; j--) {
 
-				System.out.print(c);
+				System.out.print(b++ % 2);
 			}
-			c = (i % 2 == 0 ? a++ : d++);
+
 			System.out.println();
 		}
 	}
--- Pattern4/Pattern58.java
+++ Pattern4/Pattern58.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
 
-public class Pattern56 {
+public class Pattern58 {
 	public static void main(String[] args) {
 		int i, j, length;
 		Scanner in = new Scanner(System.in);
 		System.out.println("enter your length");
 		length = in.nextInt() + 1;
-		char a = 'a';
-		int b = 1;
-		char c = (char) (b + '0');
-		char d = (char) (b + '1');
+		int a = 0;
+
 		for (i = 0; i <= length; i++) {
 			for (int k = 0; k <= i; k++) {
 				System.out.print(" ");
 			}
 			for (j = length - i; j != 0; j--) {
+				if (i % 2 != 0) {
+					System.out.print(a);
+				} else {
 
-				System.out.print(c);
+					System.out.print("*");
+				}
 			}
-			c = (i % 2 == 0 ? a++ : d++);
+			a = i % 2 != 0 ? 1 : a + 1;
+
 			System.out.println();
 		}
 	}
