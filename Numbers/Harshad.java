ecd5511abaca2d1b7a980d24226279bef42c2d7a
Kunal Dabhane <kunaldabhane20012@gmail.com>
2023-01-05 01:29:30 +0530

    harshad number

--- Numbers/Harshad.java
+++ Numbers/Harshad.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 class Harshad {
 	public static void main(String arg[]) {
 		Scanner sc = new Scanner(System.in);
-
+		int n = sc.nextInt();
+		int sum = 0;
+        int num = n;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        if (num % sum == 0) {
+            System.out.println("harshad");
+            return;
+        }
+        System.out.println("not harshad");
 	}
 }
